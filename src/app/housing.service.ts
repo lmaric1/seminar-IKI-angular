5e559994595e0bbe7155a4850622ef1134cb7d38
lmaric1 <93379077+lmaric1@users.noreply.github.com>
2023-11-22 18:31:23 +0100

    Add files via upload

---
+++ src/app/housing.service.ts
@@ -0,0 +1,92 @@
+import {Injectable} from '@angular/core';
+import {HousingLocation} from './housinglocation';
+@Injectable({
+  providedIn: 'root',
+})
+export class HousingService {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+  protected housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Hotel Hajduk',
+      city: 'Split',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'Hotel More',
+      city: 'Dubrovnik',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'Hotel Val',
+      city: 'Zadar',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 3,
+      name: 'Hotel Sunce',
+      city: 'Rijeka',
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 4,
+      name: 'Hotel Arena',
+      city: 'Pula',
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 5,
+      name: 'Hotel Rak',
+      city: 'Šibenik',
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 6,
+      name: 'Hotel Grad',
+      city: 'Korčula',
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
+      availableUnits: 5,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 7,
+      name: 'Hotel Ipsilon',
+      city: 'Umag',
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    }
+    
+  ];
+
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+  }
+}
---
+++ src/app/housinglocation.ts
@@ -0,0 +1,9 @@
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
